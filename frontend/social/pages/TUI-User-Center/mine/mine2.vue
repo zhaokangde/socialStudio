a47b3c7ccbcace6b772bd7c7af0a706172ae1f05
zhaokangde <zhaokangde@qq.com>
2022-01-28 21:29:37 +0800

    332

--- frontend/social/pages/TUI-User-Center/mine/mine2.vue
+++ frontend/social/pages/TUI-User-Center/mine/mine2.vue
@@ -2,40 +2,77 @@
   <view>
     <view>
       <!-- 第一部分 背景-->
-      <view class="view">
+      <view class="view" >
         <image
           class="image-radius"
           :mode="'aspectFill'"
           :src="'/static/static/images/背景图片.png'"
         ></image>
       </view>
-      <!-- 第二部分 纵向布局 头像 用户信息-->
+      <!-- 第二部分 纵向布局 头像 粉丝 关注 局数-->
       <view class="view-item">
         <image
           class="view-item-image"
           :src="'/static/static/images/背景图片.png'"
         ></image>
-        <view class="view-user">   <image class="view-icon"
-          :src="'/static/static/social/粉丝.png'"
-        ></image> 
-         <text class="view-icon-text" >粉丝</text>
-
- 
+        <view class="view-user">
+          <image
+            class="view-icon"
+            :src="'/static/static/social/粉丝.png'"
+          ></image>
+          <text class="view-icon-text">粉丝</text>
         </view>
-    
-        <view class="view-user">  <image class="view-icon"
-          :src="'/static/static/social/关注.png'"
-        ></image>
-        <text class="view-icon-text" >关注</text>
 
- </view>
-        <view class="view-user"> <image class="view-icon"
-          :src="'/static/static/social/局数.png'"
-        ></image>
-        <text class="view-icon-text" >局数</text>
-
-  </view>
+        <view class="view-user">
+          <image
+            class="view-icon"
+            :src="'/static/static/social/关注.png'"
+          ></image>
+          <text class="view-icon-text">关注</text>
+        </view>
+        <view class="view-user">
+          <image
+            class="view-icon"
+            :src="'/static/static/social/局数.png'"
+          ></image>
+          <text class="view-icon-text">局数</text>
+        </view>
       </view>
+      <!-- 第三部分  用户信息+编辑按钮-->
+      <view class="view-user-info-and-edit">
+        <view class="view-user-info">
+          <text class="user-info-name">比尔盖茨东北</text>
+          <text class="user-info-sign-name"
+            >这是一句个性xxxxxxxxxXXXXXXXXXXXXXXX</text
+          >
+
+          <view class="view-user-lable">
+            <view class="view-user-lable-item">
+              <image
+                class="view-lable"
+                :src="'/static/static/social/地址.png'"
+              ></image>
+              <text class="view-lable-text">杭州市</text>
+            </view>
+            <view class="view-user-lable-item">
+              <image
+                class="view-lable"
+                :src="'/static/static/social/星座.png'"
+              ></image>
+              <text class="view-lable-text">双鱼座</text>
+            </view>
+          </view>
+        </view>
+        <view class="view-user-info-edit">
+          <text style="font-size: 22upx; margin-left: 30upx">编辑</text>
+        </view>
+      </view>
+      <!-- 第四部分  我发起的，参与的 动态-->
+      <view class="view-user-game">
+      
+<v-tabs v-model="current" :scroll=true  :fontSize="'32upx'" :lineColor="' rgb(8, 8, 8)'" :color="'#rgb(77, 71, 71)'" :activeColor="'#rgb(77, 71, 71)'" :bold=true :tabs="tabs" @change="changeTab"></v-tabs>
+      </view>
+   
     </view>
   </view>
 </template>
@@ -48,6 +85,8 @@ const app = getApp();
 export default {
   data() {
     return {
+       current: 0,
+      tabs: ['我发起的', '我参与的'],
       userInfo: {
         //用户ID
         userID: "",
@@ -80,6 +119,9 @@ export default {
   },
 
   methods: {
+     changeTab(index) {
+      console.log('当前选中索引：' + index)
+    },
     setData(obj) {
       let that = this;
       let keys = [];
